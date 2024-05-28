2ce673b3df9a1e6c2fd7d24d49eb7ac9b4e3952c
dankinsoid <30962149+dankinsoid@users.noreply.github.com>
2024-05-28 22:53:25 +0400

    7.3.0

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
      targets: [
         .binaryTarget(
             name: "IronSource",
-            url: "https://github.com/ironsource-mobile/iOS-sdk/raw/master/7.2.6/IronSource7.2.6.zip",
-            checksum: "a028a1b2db6293c7b5715156dc3ebede5a1f79c49dd119f6d8bd25a2cc1a9cf4"
+            url: "https://github.com/ironsource-mobile/iOS-sdk/tree/master/8.1.0/IronSource8.1.0.zip",
+            checksum: "2af1acc57245a9e253f6fa61981cc3b48882c7767b9951e4fb70c4580ad509c9"
         )
     ]
 )
