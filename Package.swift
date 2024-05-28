6f7f04fbd0f0c10695a422a874692c9cbe657c5c
dankinsoid <30962149+dankinsoid@users.noreply.github.com>
2024-05-28 23:22:27 +0400

    8.1.2

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .iOS(.v11),
     ],
     products: [
-        .library(name: "IronSource", targets: ["IronSource"]),
+        .library(name: "IronSource", targets: ["IronSource", "IronSourceAdQualitySDK"]),
     ],
     dependencies: [
     ],
@@ -18,6 +18,11 @@ let package = Package(
             name: "IronSource",
             url: "https://github.com/ironsource-mobile/iOS-sdk/raw/master/8.1.0/IronSource8.1.0.zip",
             checksum: "2af1acc57245a9e253f6fa61981cc3b48882c7767b9951e4fb70c4580ad509c9"
+        ),
+        .binaryTarget(
+            name: "IronSourceAdQualitySDK",
+            url: "https://github.com/ironsource-mobile/iOS-adqualitysdk/releases/download/IronSource_AdQuality_7.14.2/IronSourceAdQualitySDK-ios-v7.14.2.zip",
+            checksum: "05f6f206148fd84c3bded71ef27c22448fedf555555c69cfcefccf9cf8a4fd0b"
         )
     ]
 )
